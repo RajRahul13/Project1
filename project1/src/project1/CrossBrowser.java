4bb60fb27c666dab88351d46818845a7d09b3aa6
M1053553 <M1053553@A2ML35294.mindtree.com>
2023-05-24 23:46:08 +0530

    change is crossbrowser pgm

--- project1/src/project1/CrossBrowser.java
+++ project1/src/project1/CrossBrowser.java
@@ -7,7 +7,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.CapabilityType;
 
 /**
  * @author M1053553
@@ -34,12 +37,16 @@ public class CrossBrowser {
 	public static void setBrowserConfig() {
 		if (browser=="Chrome") {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\M1053553\\Desktop\\TestAutomation\\Drivers\\chromedriver.exe");
-		driver= new ChromeDriver();
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		driver= new ChromeDriver(options);
 		}
 		
 		if (browser=="EdgeDriver") {
 		System.setProperty("webdriver.edge.driver", "C:\\Users\\M1053553\\Desktop\\TestAutomation\\Drivers\\msedgedriver.exe");
-		driver = new EdgeDriver();
+		EdgeOptions options=new EdgeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		driver = new EdgeDriver(options);
 		}
 	}
 	public static void runTest() {
--- project1/src/project1/LaunchChromeBrowser.java
+++ project1/src/project1/LaunchChromeBrowser.java
@@ -1,17 +1,19 @@
 package project1;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 public class LaunchChromeBrowser{
    public static void main(String[] args) throws InterruptedException  {
      
       // browser type and chromedrover.exe path as parameters
-      System.setProperty("webdriver.chrome.driver",
-      "C:\\Users\\M1053553\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
-      WebDriver driver = new ChromeDriver();
-      String url = " https://www.google.com";
-      driver.get(url);
-      Thread.sleep(2000);
-      driver.quit();
+	   System.setProperty("webdriver.chrome.driver","C:\\Users\\M1053553\\Desktop\\TestAutomation\\Drivers\\chromedriver.exe");
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        String url = " https://www.google.com";
+        driver.get(url);
+        Thread.sleep(2000);
+        driver.quit();
       
    }
 }
